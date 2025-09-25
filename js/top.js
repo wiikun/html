7b1fdd7dda7f6ffe62caa7662dc2f983c59b42b0
wiikun <qwop90175@gmail.com>
2025-09-25 19:26:32 +0900

    update

---
+++ js/top.js
@@ -0,0 +1,8 @@
+const handler = () => {
+    const flag = window.confirm("隠しのブログテンプレートページに行きますか？")
+    if(flag){
+        window.location = "blog/template.html"
+    }
+}
+
+document.querySelector("#ivst").addEventListener("click",handler)
