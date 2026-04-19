cd29ad37eed161739451730815db8bd757df9fa3
wiikun <qwop90175@gmail.com>
2026-04-19 10:41:46 +0900

    jquery added

--- js/top.js
+++ js/top.js
@@ -1,8 +1,9 @@
-const handler = () => {
+$(() => {const handler = () => {
     const flag = window.confirm("隠しのブログテンプレートページに行きますか？");
     if(flag){
         window.location = "blog/template.html";
     }
 }
 
-document.querySelector("#ivst").addEventListener("click",handler);
+$("#ivst").click(handler)}
+)
